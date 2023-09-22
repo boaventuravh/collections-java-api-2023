60fca78cf8f89c030793d21e9249f13b578873b0
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-09-22 16:05:46 -0300

    catálogo de livros concluído

--- pesquisa-list/src/catalogo/CatalogoLivro.java
+++ pesquisa-list/src/catalogo/CatalogoLivro.java
@@ -1,2 +1,50 @@
-package catalogo;public class CatalogoLivro {
+package catalogo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivro {
+    private List<Livro> acervoLivros;
+
+    public CatalogoLivro(){
+        acervoLivros = new ArrayList<>();
+    }
+
+    public List<Livro> getAcervoLivros() {
+        return acervoLivros;
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        acervoLivros.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosEncontrados = new ArrayList<>();
+
+        for(Livro li : acervoLivros)
+            if (li.getAutor().equalsIgnoreCase(autor))
+                livrosEncontrados.add(li);
+
+        return livrosEncontrados;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosIntervalo = new ArrayList<>();
+
+        for (Livro li : acervoLivros)
+            if (li.getAnoPublicação() >= anoInicial && li.getAnoPublicação() <= anoFinal)
+                livrosIntervalo.add(li);
+
+        return livrosIntervalo;
+    }
+
+    //Esse método pesquisarPorTitulo() pode ganhar em performance utilizando a busca
+    //binária. É algo para se ter em mente e implementar mais adiante
+    public Livro pesquisarPorTitulo(String titulo){
+        for (Livro li : acervoLivros)
+            if (li.getTitulo().equalsIgnoreCase(titulo))
+                return li;
+
+        return null;
+    }
 }
--- pesquisa-list/src/catalogo/Livro.java
+++ pesquisa-list/src/catalogo/Livro.java
@@ -1,2 +1,25 @@
-package catalogo;public class Livro {
+package catalogo;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicação;
+
+    public Livro(String titulo, String autor, int anoPublicação){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicação = anoPublicação;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicação() {
+        return anoPublicação;
+    }
 }
