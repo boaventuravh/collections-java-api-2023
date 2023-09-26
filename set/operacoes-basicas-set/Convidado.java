8089fde2832b3cbbb125bc8f42def545c21e17b6
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-09-26 15:40:40 -0300

    alterações no README + init operações básicas

--- set/operacoes-basicas-set/Convidado.java
+++ set/operacoes-basicas-set/Convidado.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Convidado {
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite){
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
 }
