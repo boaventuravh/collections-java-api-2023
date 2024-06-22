6e51f42acc5537ab6be8e0ce41c8989d5205525a
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-06-22 18:29:19 -0300

    exercícios iniciais

---
+++ set/src/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,54 @@
+package OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    public Set<Convidado> convidados;
+
+    public ConjuntoConvidados() {
+        this.convidados = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidados.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for (Convidado co : convidados) {
+            if (co.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = co;
+                break;
+            }
+        }
+        convidados.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidados.size();
+    }
+
+    public void exibirConvidados(){
+        for (Convidado co : convidados) {
+            System.out.println(co.getNome() + ", " + co.getCodigoConvite());
+        }
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("João", 1);
+        conjuntoConvidados.adicionarConvidado("Maria", 2);
+        conjuntoConvidados.adicionarConvidado("Alfredo", 3);
+        conjuntoConvidados.adicionarConvidado("Joana", 4);
+
+        conjuntoConvidados.exibirConvidados();
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(2);
+
+        conjuntoConvidados.exibirConvidados();
+        System.out.println("Total de convidados: " + conjuntoConvidados.contarConvidados());
+    }
+}
