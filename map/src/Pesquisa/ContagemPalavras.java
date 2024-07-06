441ed2917fad0818cb242f8d34577dddd85360b2
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-07-06 12:00:57 -0300

    acrescentada função encontrarPalavraMaisFrequente() e testes

--- map/src/Pesquisa/ContagemPalavras.java
+++ map/src/Pesquisa/ContagemPalavras.java
@@ -25,5 +25,35 @@ public class ContagemPalavras {
         System.out.println(contagemPalavrasMap);
     }
 
+    public HashMap<String, Integer> encontrarPalavraMaisFrequente(){
+        HashMap<String, Integer> palavraMaisFrequenteContagem = new HashMap<>();
+        String palavraMaisFrequente = "";
+        Integer maiorContagem = Integer.MIN_VALUE;
+        palavraMaisFrequenteContagem.put(palavraMaisFrequente, maiorContagem);
+        if (!contagemPalavrasMap.isEmpty()){
+            for (String chave : contagemPalavrasMap.keySet()){
+                if (contagemPalavrasMap.get(chave) > palavraMaisFrequenteContagem.get(palavraMaisFrequente)){
+                    palavraMaisFrequenteContagem.remove(palavraMaisFrequente);
+                    palavraMaisFrequente = chave;
+                    maiorContagem = contagemPalavrasMap.get(chave);
+                    palavraMaisFrequenteContagem.put(palavraMaisFrequente, maiorContagem);
+                }
+            }
+        }
+        return palavraMaisFrequenteContagem;
+    }
+    public static void main(String[] args) {
+        ContagemPalavras contagemPalavras = new ContagemPalavras();
+
+        contagemPalavras.adicionarPalavra("Jurema", 1);
+        contagemPalavras.adicionarPalavra("Carlos", 4);
+        contagemPalavras.adicionarPalavra("João", 8);
+        contagemPalavras.adicionarPalavra("José", 3);
+
+        contagemPalavras.exibirContagemPalavras();
+
+        System.out.println("Palavra mais frequente: " + contagemPalavras.encontrarPalavraMaisFrequente());
+
+    }
 
 }
